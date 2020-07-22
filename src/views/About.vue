1c6aba943b9def3011544084e043b40f3d21f9bf
annakky <annakky@naver.com>
2020-07-22 22:48:16 +0900

    Game UI 추가

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,4 +11,4 @@ export default {
   },
   
 }
-</script>
+</script scoped>
--- src/views/ActivityChoose.vue
+++ src/views/ActivityChoose.vue
@@ -1,8 +1,10 @@
 <template>
 <div>
-    <v-container class="d-flex justify-center mb-6">
-    <v-btn class="ma-2" x-large color="warning" dark>퀴즈게임</v-btn>
-    <v-btn class="ma-2" x-large color="primary" dark @click="mindmapClicked">마인드맵</v-btn>
+    <v-container class="d-flex justify-center mb-6" style="width:100%; height:100%;">
+    <v-btn id="btnL" v-if="clicked" color="warning" dark @click="quizClicked">퀴즈게임</v-btn>
+    <v-btn id="btnS" v-if="!clicked" color="warning" dark @click="friendClicked">친구와 하기</v-btn>
+    <v-btn id="btnS" v-if="!clicked" color="warning" dark @click="botClicked">Bot과 하기</v-btn>
+    <v-btn id="btnL" color="primary" dark @click="mindmapClicked">마인드맵</v-btn>
     </v-container>
 </div>
 </template>
@@ -11,17 +13,38 @@
 export default {
   data () {
     return {
+      clicked: true,
     }
   },
   methods: {
     mindmapClicked() {
       var router = this.$router;
-      router.push({ name: 'MindMap' , params: { bookId: this.$route.params.bookId}});
-    }
+      router.replace({ name: 'MindMap' , params: { bookId: this.$route.params.bookId}});
+    },
+    quizClicked() {
+      this.clicked = !this.clicked;
+    },
+    friendClicked() {
+      var router = this.$router;
+      router.replace({name: 'GameWF'});
+    },
+    botClicked() {
+      var router = this.$router;
+      router.replace({name: 'GameWB', params: { bookId: this.$route.params.bookId}});
+    },
   },
 }
 </script>
 
-<style>
-
+<style scoped>
+#btnL {
+  width: 300px;
+  height: 100px;
+  margin:20px
+}
+#btnS {
+  width: 130px;
+  height: 100px;
+  margin:20px;
+}
 </style>
