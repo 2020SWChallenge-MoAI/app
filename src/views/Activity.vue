47d94702086a9874de8eed9c7578b910abcb6316
annakky <annakky@naver.com>
2020-07-23 23:48:12 +0900

    BookList Vue 분리

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -1,49 +1,31 @@
 <template>
   <div>
-    <v-container class="d-flex justify-space-around mb-6">
-        <v-col cols="12">
-          <v-row>
-            <v-card
-              v-for="book in books"
-              :key="book.id"
-              :style="{border: '1px solid black', textAlign: 'center'}"
-              class="book_list ma-6 pa-14"
-              outlined
-              width=240px
-              height="200px"
-              @click="bookClicked(book.id, book.title)"
-            >
-            {{ book.title }}
-            </v-card>
-          </v-row>
-        </v-col>
-    </v-container>
+    <BookList @bookId="getBookId" @bookTitle="getBookTitle"/>
   </div>
 </template>
 
 <script>
+import BookList from '../components/BookList.vue';
+
 export default {
+  components: {
+    BookList,
+  },
   data () {
     return {
-      books: [
-        { id: 1, title: "신데렐라" },
-        { id: 2, title: "백설공주" },
-        { id: 3, title: "흥부와 놀부" },
-        { id: 4, title: "홍길동전" },
-        { id: 5, title: "토끼와 거북이" },
-        { id: 6, title: "파브르 위인전" },
-        { id: 7, title: "삼국지" },
-        { id: 8, title: "마음의 소리" },
-        { id: 9, title: "집이 없어" },
-        { id: 10, title: "신의 탑" },
-      ]
+      bookid: 0,
+      booktitle: '',
     }
   },
   methods: {
-    bookClicked(bid, btitle) {
+    getBookId(id) {
+      this.bookid = id;
+    },
+    getBookTitle(title) {
+      this.booktitle = title;
       var router = this.$router;
-      router.push({ name: 'ActivityChoose' , params: { bookId: bid, bookTitle: btitle }});
-    }
+      router.push({ name: 'ActivityChoose' , params: { bookId: this.bookid, bookTitle: this.booktitle }});
+    }   
   },
 }
 </script>
--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-
-      <v-container class="d-flex justify-center mb-6">
-          <h1>책 Data</h1>
-      </v-container>
-
+    <BookList />
   </div>
 </template>
 
 <script>
+import BookList from '../components/BookList.vue';
+
 export default {
+  components: {
+    BookList,
+  },
 
 }
 </script>
